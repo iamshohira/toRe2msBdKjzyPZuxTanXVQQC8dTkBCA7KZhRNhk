e6c5faf6fc8177d1e9a3b06cd042459c9908808c
iamshohira <hiraide@cheme.kyoto-u.ac.jp>
2022-08-16 14:58:16 +0900

    deal unknown commit

--- update_checker.py
+++ update_checker.py
@@ -15,6 +15,9 @@ class UpdateChecker:
             return
         self.repo = git.Repo(os.path.join(os.path.dirname(sys.argv[0])))
         self.current_tag = next((tag for tag in self.repo.tags if tag.commit == self.repo.head.commit), None)
+        if self.current_tag == None:
+            self.header += f"Unknown version: {self.repo.head.commit.hexsha}\n"
+            return
         self.header += f"current version: {self.current_tag.name}"
         origin = self.repo.remote()
         try:
